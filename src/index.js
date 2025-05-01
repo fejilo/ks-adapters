372ab16188cf7536ff2e96de58221e65a604fc53
nagato <fjimenezlozano@gmail.com>
2025-05-01 10:15:05 -0500

    include Data adpater

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import Auth from "./Auth/Auth.js";
 import Cache from "./Cache/Cache.js";
+import Data from "./Data/Data.js";
 import Event from "./Event/Event.js";
 import Socket from "./Socket/Socket.js";
 
-export { Auth, Cache, Event, Socket };
+export { Auth, Cache, Data, Event, Socket };
 
 export default {
   Auth,
   Cache,
+  Data,
   Event,
   Socket,
 };
